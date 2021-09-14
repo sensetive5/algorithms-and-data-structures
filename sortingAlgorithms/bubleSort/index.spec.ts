c5ca24b4775222bb0e67cd202136d0530e41416e
Alexander Sokolov <owngr@yandex-team.ru>
2021-09-14 20:17:51 +0300

    Optimize bubble sort

--- sortingAlgorithms/bubleSort/index.spec.ts
+++ sortingAlgorithms/bubleSort/index.spec.ts
@@ -1,9 +1,19 @@
-import {bubbleSort} from "./index";
+import {bubbleSort, bubbleSortOptimized} from "./index";
 
 describe('bubbleSort', () => {
-    it('Should sort list of numbers ascending numeric order', () => {
-        const expectedResult = bubbleSort([-1, 10, -10, 15, 4, 2, 3, 6, 115, 100, 95]);
+    describe('bubbleSort', () => {
+        it('Should sort list of numbers ascending numeric order', () => {
+            const expectedResult = bubbleSort([-1, 10, -10, 15, 4, 2, 3, 6, 115, 100, 95]);
 
-        expect(expectedResult).toStrictEqual([-10, -1, 2, 3, 4, 6, 10, 15, 95, 100, 115]);
+            expect(expectedResult).toStrictEqual([-10, -1, 2, 3, 4, 6, 10, 15, 95, 100, 115]);
+        });
+    });
+
+    describe('bubbleSortOptimized', () => {
+        it('Should sort list of numbers ascending numeric order', () => {
+            const expectedResult = bubbleSortOptimized([8, 1, 2, 3, 4, 5, 6, 7]);
+
+            expect(expectedResult).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+        });
     });
 });
--- sortingAlgorithms/bubleSort/index.ts
+++ sortingAlgorithms/bubleSort/index.ts
@@ -19,3 +19,27 @@ export function bubbleSort (numbersList: number[]): number[] {
 
     return numbersList;
 }
+
+export function bubbleSortOptimized (numbersList: number[]): number[] {
+    let noSwaps: boolean;
+
+    for (let i = 0; i < numbersList.length; i++) {
+        noSwaps = true;
+
+        for (let j = 0; j < numbersList.length; j++) {
+            const nextIdx = j + 1;
+            const current = numbersList[j];
+            const next = numbersList[nextIdx];
+
+            if (current > next) {
+                swapTwo(numbersList, j, nextIdx);
+                noSwaps = false;
+            }
+        }
+
+        if (noSwaps) break;
+    }
+
+    return numbersList;
+}
+
